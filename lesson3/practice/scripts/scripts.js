7da8be58ee1e6d425e69628942b171bac29bba7e
HUANG37W <huang78@byui.edu>
2022-01-28 12:11:13 -0700

    commit

--- lesson3/practice/scripts/scripts.js
+++ lesson3/practice/scripts/scripts.js
@@ -1,11 +1,11 @@
 //const button = document.querySelector('.btn');
-const buttons = document.getElementsByClassName('btn');
+const button = document.getElementsByClassName('btn');
 
-console.log(buttons[0]);
+console.log(button[1]);
 
 function show() {
     //button.classList.add('newstyle');
-    //button.classList.toggle('newstyle');
+    button.classList.toggle('newstyle');
 };
 
 
@@ -13,3 +13,26 @@ button.addEventListener('click', show);
 
 // alternative way to use arrow function 
 // button.addEventListener('click', ()=> {button.classList.add('newstyle')});
+
+
+const hamburgerBtn = document.querySelector('.ham');
+const navigation = document.querySelector('.navigation');
+
+hamburgerBtn.addEventListener('click', () => {navigation.classList.toggle('responsive')}), false;
+
+let w_names = ['Sunday'
+, 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday', 'Saturday'];
+let m_names = ['January' , 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+'September', 'October', 'November'
+, 'December'];
+let mydate = new Datel);
+console. log(mydate);
+
+
+let weekname = w_names[mydate.getDay()];
+console.log(mydate.getDay());
+let monthname = m_names[mydate.getMonth()];
+
+const dateouput = document.querySelector('#date');
+
+dateoutput.textContent = weekname + "," + mydate.getDate() + " " + monthname + " " + mydate. getFullYear();
