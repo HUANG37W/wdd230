0503a829abdf8fead33a4dc26d97284171e90d6c
HUANG37W <huang78@byui.edu>
2022-01-28 11:28:26 -0700

    commit

--- lesson3/practice/scripts/scripts.js
+++ lesson3/practice/scripts/scripts.js
@@ -1,8 +1,13 @@
-const button = document.querySelector('.btn');
+//const button = document.querySelector('.btn');
+const buttons = document.getElementsByClassName('btn');
+
+console.log(buttons[0]);
 
 function show() {
-    button.classList.add('newstyle');
+    //button.classList.add('newstyle');
+    //button.classList.toggle('newstyle');
 };
+
 
 button.addEventListener('click', show);
 
