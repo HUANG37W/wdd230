9c6ee560ffb083b2deba373ac601bc9cb1e62d32
HUANG37W <huang78@byui.edu>
2022-02-01 09:37:08 -0700

    commit

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,52 +1,64 @@
-window.addEventListener("load", () => {
-    const hamButton = document.querySelector(".ham");
-    const mainNav = document.querySelector("#navigation");
+// window.addEventListener("load", () => {
+//     const hamButton = document.querySelector(".ham");
+//     const mainNav = document.querySelector(".navigation");
   
-    hamButton.addEventListener(
-      "click",
-      () => {
-        mainNav.classList.toggle("responsive");
-      },
-      false
-    );
-  });
+//     hamButton.addEventListener(
+//       "click",
+//       () => {
+//         mainNav.classList.toggle("responsive");
+//       },
+//       false
+//     );
+//   });
+
+
+    function toggleMenu() {
+      const mainNav = document.querySelector(".navigation");
+      mainNav.classList.toggle("responsive");
+    }
+  const todayDate = document.getElementById("date");
+
+  const now = new Date();
+  const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+  
+    todayDate.innerHTML = fullDate;
 
   //const button = document.querySelector('.btn');
-const button = document.getElementsByClassName('btn');
+//const button = document.getElementsByClassName('btn');
 
-console.log(button[1]);
+// console.log(button[1]);
 
-function show() {
-    //button.classList.add('newstyle');
-    button.classList.toggle('newstyle');
-};
-
-
-button.addEventListener('click', show);
-
-// alternative way to use arrow function 
-// button.addEventListener('click', ()=> {button.classList.add('newstyle')});
+// function show() {
+//     //button.classList.add('newstyle');
+//     button.classList.toggle('newstyle');
+// };
 
 
-const hamburgerBtn = document.querySelector('.ham');
-const navigation = document.querySelector('.navigation');
+// button.addEventListener('click', show);
 
-hamburgerBtn.addEventListener('click', () => {navigation.classList.toggle('responsive')}), false;
-
-let w_names = ['Sunday'
-, 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday', 'Saturday'];
-let m_names = ['January' , 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-'September', 'October', 'November'
-, 'December'];
-let mydate = new Date();
-console. log(mydate);
+// // alternative way to use arrow function 
+// // button.addEventListener('click', ()=> {button.classList.add('newstyle')});
 
 
-let weekname = w_names[mydate.getDay()];
-console.log(mydate.getDay());
-let monthname = m_names[mydate.getMonth()];
+// const hamburgerBtn = document.querySelector('.ham');
+// const navigation = document.querySelector('.navigation');
 
-const dateouput = document.querySelector('#date');
+// hamburgerBtn.addEventListener('click', () => {navigation.classList.toggle('responsive')}), false;
 
-dateoutput.textContent = weekname + "," + mydate.getDate() + " " + monthname + " " + mydate. getFullYear();
+// let w_names = ['Sunday'
+// , 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday', 'Saturday'];
+// let m_names = ['January' , 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+// 'September', 'October', 'November'
+// , 'December'];
+// let mydate = new Date();
+// console. log(mydate);
+
+
+// let weekname = w_names[mydate.getDay()];
+// console.log(mydate.getDay());
+// let monthname = m_names[mydate.getMonth()];
+
+// const dateouput = document.querySelector('#date');
+
+// dateoutput.textContent = weekname + "," + mydate.getDate() + " " + monthname + " " + mydate. getFullYear();
   
