3b5230e2e6b34850e1a17b55df9123c6a6a8172d
HUANG37W <huang78@byui.edu>
2022-01-31 20:08:00 -0700

    commit

---
+++ chamber/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const hamButton = document.querySelector(".ham");
+    const mainNav = document.querySelector("#navigation");
+  
+    hamButton.addEventListener(
+      "click",
+      () => {
+        mainNav.classList.toggle("responsive");
+      },
+      false
+    );
+  });
+  
