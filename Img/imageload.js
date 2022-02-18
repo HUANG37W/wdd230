9b5730da3264c84628257d592625071a85d86ce4
Jason Huang <huang78@byui.edu>
2022-02-17 21:19:50 -0700

    commit

--- Img/imageload.js
+++ Img/imageload.js
@@ -1,16 +1,28 @@
-const images = document.querySelector('img');
+// const images = document.querySelector('img');
 
-const options = { threshold: [.5] }
+// const options = { threshold: [.5] }
 
-const io = new IntersectionObserver(
-    (entries, io) => {
-        entries.forEach(entry => {
-        console.log(entries);
-        });  
-    }, options);
+// const io = new IntersectionObserver(
+//     (entries, io) => {
+//         entries.forEach(entry => {
+//         console.log(entries);
+//         });  
+//     }, options);
 
 
-images.forEach(images => {
-    io.observe(images);
-});
+// images.forEach(images => {
+//     io.observe(images);
+// });
 
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
