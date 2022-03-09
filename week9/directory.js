a9a2fecbc51adc739b053d0f063919db18923688
Jason Huang <huang78@byui.edu>
2022-03-09 16:28:19 -0700

    commit

--- week9/directory.js
+++ week9/directory.js
@@ -8,20 +8,25 @@ fetch(requestURL)
     console.log(jsonObject);  // temporary checking for valid response and data parsing
     const companies = jsonObject['companies'];
     companies.forEach(displayCompanies);
-    //console.log(businesses[0]);
-    //cards();
   });
 
   function displayCompanies(company) {  
     // Create elements to add to the document
     let card = document.createElement('section');
+    let list = document.createElement('section');
+    let listH2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the prophet's full name
+    let listAdr = document.createElement('p');
+    let listPhn = document.createElement('p');
+    let img = document.createElement('img');
     let h2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the prophet's full name
     let adr = document.createElement('p');
     let phn = document.createElement('p');
-    let img = document.createElement('img');
     h2.textContent = `${company.name} ${company.lastname}`;
     adr.textContent = `${company.address}`;
     phn.textContent = `${company.phone}`;
+    listH2.textContent = `${company.name} ${company.lastname}`;
+    listAdr.textContent = `${company.address}`;
+    listPhn.textContent = `${company.phone}`;
     img.setAttribute('src', company.imageurl);
     img.setAttribute('alt', `${company.name} ${company.lastname} ${company.order}`);
 
@@ -30,33 +35,19 @@ fetch(requestURL)
     card.appendChild(adr);
     card.appendChild(phn);
     card.appendChild(img);
+    list.appendChild(listH2);
+    list.appendChild(listAdr);
+    list.appendChild(listPhn);
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
   }
 
-//to clear out cards
-//let cards = document.querySelectorAll('.cards')
-//cards.forEach((items)=>{
-//items.remove();
-//})
+const cardsButton = document.querySelector(".cards-button");
+const listsButton = document.querySelector(".lists-button");
 
-function changeStyles() {
-//changing styles in JS
-if (window.innerWidth > 800 && window,innerWidth < 1000){
-let change = document.querySelector('.cards');
-change.setAttribute('style','background-color: pink; border: 5px solid yellow; display: block;')
- }
+const Cards = document.querySelector(".cards");
+const Lists = document.querySelector(".lists");
+
+cardsButton.onclick = function(){
+  
 }
-// event listeners 
-
-window.addEventListener('resize', changeStyles);
-//window.addEventListener('load', changeStyles);
-//function cards(){
-  //const card = document.createElement('div');
-  //card.className = 'company';
-  //const box = document.querySelector('.cards');
-  //box.appendChild(card);
-  //const img = document.createElement('img');
-  //img.className = 'company-logo';
-  //box.appendChild(img);
-//}
